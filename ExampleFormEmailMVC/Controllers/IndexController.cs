5c6d4c4b9dd6a15d49ed19e10203b20cb8fd60f4
tridon777 <tridon777@gmail.com>
2016-10-21 01:46:31 -0500

    More Infastructure
    -Finishing up scaffolding model, view , layout, and packages
    
    -Allowing myself to use one 'n' version package - Bootstrap v4-alpha5

--- ExampleFormEmailMVC/Controllers/IndexController.cs
+++ ExampleFormEmailMVC/Controllers/IndexController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ExampleFormEmailMVC.Models;
 
 namespace ExampleFormEmailMVC.Controllers
 {
@@ -10,10 +11,25 @@ namespace ExampleFormEmailMVC.Controllers
     public class IndexController : Controller
     {
         [HttpGet]
-        [Route("index")]
+        [Route("Index")]
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        [Route("GenericForm")]
+        public ActionResult GenericForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("GenericForm")]
+        public ActionResult GenericForm(GenericFormModel model)
+        {
+            return View();
+        }
+
     }
 }
---
+++ ExampleFormEmailMVC/Models/GenericFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace ExampleFormEmailMVC.Models
+{
+    public class GenericFormModel
+    {
+        [Required(ErrorMessage = "Field is required")]
+        public string ExampleName { get; set; }
+        public string ExampleAddress { get; set; }
+        public string ExamplePhoneNumber { get; set; }
+        public string ExampleDate { get; set; }
+    }
+}
